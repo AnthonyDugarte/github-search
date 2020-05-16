d400b49a214a1f3908205d83c9051a2a4a040c8e
Anthony Dugarte <toonny1998@gmail.com>
2020-05-16 14:21:10 -0400

    Add simple layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <div className="container mx-auto h-screen flex flex-col p-8">
+      <h1 className="text-4xl sm:text-6xl md:text-6xl text-red-500">
+        GitHub Search
+      </h1>
+    </div>
+  )
+}
