c4a85352a688ee5dc485d7fe59feb1734a2789fa
Anthony Dugarte <toonny1998@gmail.com>
2020-05-17 16:11:52 -0400

    Show users list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
+import { PageProps } from "gatsby"
 
-export default () => {
+import UserList from "../components/user-list"
+import { UserData } from "../interfaces"
+
+export default (props: PageProps) => {
+  const [data, setData] = useState<UserData[]>([])
+
   return (
     <div className="container mx-auto h-screen flex flex-col p-8">
       <h1 className="text-4xl sm:text-6xl md:text-6xl text-red-500">
         GitHub Search
       </h1>
+
+      <UserList data={data} />
     </div>
   )
 }
