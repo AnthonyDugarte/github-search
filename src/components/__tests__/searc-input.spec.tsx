fe201f5c51f0f582d0d43b3f649b68db353e6ce0
Anthony Dugarte <toonny1998@gmail.com>
2020-05-17 14:08:30 -0400

    Add searching debounce

--- src/components/__tests__/searc-input.spec.tsx
+++ src/components/__tests__/searc-input.spec.tsx
@@ -30,15 +30,42 @@ describe("Search Input component", () => {
     expect(renderedSearchInput.asFragment()).toMatchSnapshot()
   })
 
-  test("Shows correct searched text", () => {
+  test("Correctly debounce searching", async () => {
     const search1 = faker.name.firstName()
+
+    // Dispatch simulation of input
     dispatchInputEntry(search1)
+
+    // User should see what he is typing
     expect(screen.getByTestId(githubSearchInputTestId)).toHaveValue(search1)
+
+    // No searching has been made yet, waiting for user to stop typing
+    expect(mockOnSearch.mock.calls[0]).toBeUndefined()
+    await new Promise(resolve => setTimeout(resolve, 100))
+    expect(mockOnSearch.mock.calls[0]).toBeUndefined()
+
+    // "Wait" for user to stop typing
+    await new Promise(resolve => setTimeout(resolve, 500))
+
+    // A search should have been made
     expect(mockOnSearch.mock.calls[0]?.[0]).toBe(search1)
 
+    // We are going to do another search
     const search2 = faker.name.firstName()
     dispatchInputEntry(search2)
+
+    // User shuld see his search
     expect(screen.getByTestId(githubSearchInputTestId)).toHaveValue(search2)
+
+    // But we should wait to see if he hasn't stop searching
+    expect(mockOnSearch.mock.calls[1]).toBeUndefined()
+    await new Promise(resolve => setTimeout(resolve, 100))
+    expect(mockOnSearch.mock.calls[1]).toBeUndefined()
+
+    // User stoped typing for a while...
+    await new Promise(resolve => setTimeout(resolve, 300))
+
+    // Second search should have been made
     expect(mockOnSearch.mock.calls[1]?.[0]).toBe(search2)
   })
 })
--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, useCallback } from "react"
+import { DebounceInput } from "react-debounce-input"
 
 export interface SearchInputProps {
   onSearch(search: string): void
@@ -6,16 +7,18 @@ export interface SearchInputProps {
 
 const SearchInput: FunctionComponent<SearchInputProps> = ({ onSearch }) => {
   const _onSearch = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => onSearch(e.currentTarget.value),
+    (e: React.ChangeEvent<HTMLInputElement>) => onSearch(e.target.value),
     [onSearch]
   )
 
   return (
-    <input
+    <DebounceInput
       data-testid="github-search-input"
       placeholder="Search for someone..."
       className="border focus:outline-none focus:shadow-outline rounder border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal w-full lg:w-2/3"
-      onInput={_onSearch}
+      minLength={1}
+      debounceTimeout={275} // https://humanbenchmark.com/tests/reactiontime/statistics
+      onChange={_onSearch}
     />
   )
 }
